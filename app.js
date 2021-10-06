42c2515971cdb46329a60d1ccb8c5c02e1ca5725
Troy Dorman <troydorman@gmail.com>
2021-10-06 08:51:15 -0600

    installed ejs render view engine

--- app.js
+++ app.js
@@ -1,8 +1,18 @@
 const Koa = require("koa");
 const KoaRouter = require("koa-router");
 const app = new Koa();
+const path = require("path");
+const render = require("koa-ejs");
 
 const router = new KoaRouter();
+
+render(app, {
+  root: path.join(__dirname, "views"),
+  layout: "layout",
+  viewExt: "html",
+  cache: false,
+  debug: false,
+});
 
 /* app.use(async (ctx) => (ctx.body = { msg: "Hello World" })); */
 router.get("/test", (ctx) => (ctx.body = "Hello Test"));
