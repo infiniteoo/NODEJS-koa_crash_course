ce3fd8e8b4f6b3f9eb274b6300996f121f334d1a
Troy Dorman <troydorman@gmail.com>
2021-10-06 08:41:37 -0600

    can also output json objects

--- app.js
+++ app.js
@@ -2,8 +2,6 @@ const Koa = require("koa");
 
 const app = new Koa();
 
-app.use(async (ctx) => (ctx.body = "Hello World"));
+app.use(async (ctx) => (ctx.body = { msg: "Hello World" }));
 
 app.listen(3000, () => console.log("Server started"));
-
-
